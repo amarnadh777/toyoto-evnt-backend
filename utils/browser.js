85f6a7f992d557fffce2bb6c7168bf3558c7b331
amarnadh777 <amarnadh6565@gmail.com>
2026-02-16 12:39:20 +0530

    browser change by os

--- utils/browser.js
+++ utils/browser.js
@@ -1,35 +1,64 @@
 const puppeteer = require("puppeteer");
+const os = require("os");
+const fs = require("fs");
 
 let browserInstance = null;
 
 async function getBrowser() {
+
   try {
 
     if (!browserInstance) {
 
-      console.log("Launching Puppeteer browser...");
+      console.log("Launching Puppeteer with Google Chrome...");
+
+      const platform = os.platform();
+
+      let chromePath;
+
+      if (platform === "linux") {
+
+        // Linux Chrome paths
+        if (fs.existsSync("/usr/bin/google-chrome")) {
+          chromePath = "/usr/bin/google-chrome";
+        } 
+        else if (fs.existsSync("/usr/bin/google-chrome-stable")) {
+          chromePath = "/usr/bin/google-chrome-stable";
+        }
+
+      } 
+      else if (platform === "win32") {
+
+        chromePath =
+          "C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe";
+
+      } 
+      else if (platform === "darwin") {
+
+        chromePath =
+          "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome";
+
+      }
 
       browserInstance = await puppeteer.launch({
 
         headless: true,
 
+        executablePath: chromePath, // ✅ Use Google Chrome
+
         args: [
           "--no-sandbox",
           "--disable-setuid-sandbox",
-
-          // VERY IMPORTANT for Ubuntu server
           "--disable-dev-shm-usage",
           "--disable-gpu",
           "--no-zygote",
+          "--single-process"
         ]
 
       });
 
-
-
-      
       browserInstance.on("disconnected", () => {
-        console.log("Browser disconnected. Resetting...");
+        console.log("Browser disconnected");
         browserInstance = null;
       });
 
@@ -38,9 +67,12 @@ async function getBrowser() {
     return browserInstance;
 
   } catch (err) {
+
     console.error("Browser launch error:", err);
     throw err;
+
   }
+
 }
 
 module.exports = getBrowser;
