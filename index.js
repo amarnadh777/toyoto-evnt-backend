b9f02b9e386f90fa2523aa73e509eb7d8510a671
amarnadh777 <amarnadh6565@gmail.com>
2026-02-16 12:43:22 +0530

    https change to app

--- index.js
+++ index.js
@@ -33,6 +33,11 @@ const sslOptions = {
 };
 
 // ✅ Create HTTPS server (IMPORTANT FIX)
-https.createServer(sslOptions, app).listen(port, '0.0.0.0', () => {
+// https.createServer(sslOptions, app).listen(port, '0.0.0.0', () => {
+//   console.log(`🔐 HTTPS Server running on https://localhost:${port}`);
+// });
+
+
+ app.listen(port, '0.0.0.0', () => {
   console.log(`🔐 HTTPS Server running on https://localhost:${port}`);
 });
